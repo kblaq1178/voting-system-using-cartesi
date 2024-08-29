a66c1b8c491aabba72cfec258f4585734aa22055
kblaq1178 <160018320+kblaq1178@users.noreply.github.com>
2024-08-29 03:06:43 -0700

    Update utils.js

---
+++ utils.js
@@ -0,0 +1,15 @@
+
+
+exports.isValidProposal = (title, description) => {
+    return title && title.trim() !== '' && description && description.trim() !== '';
+};
+
+exports.isValidVoter = (name, id) => {
+    return name && name.trim() !== '' && id && id.trim() !== '';
+};
+
+exports.isValidVote = (voterId, proposalId, vote) => {
+    return voterId && voterId.trim() !== '' && proposalId && proposalId.trim() !== '' && typeof vote === 'boolean';
+};
+
+
