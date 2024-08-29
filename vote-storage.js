2fe04430cccef9195f47feafb79364f31b53b627
kblaq1178 <160018320+kblaq1178@users.noreply.github.com>
2024-08-29 03:18:56 -0700

    Update vote-storage.js

---
+++ vote-storage.js
@@ -0,0 +1,13 @@
+const storage = a Map();
+
+exports.save = (item) => {
+    storage.set(item.id, item);
+};
+
+exports.getById = (id) => {
+    return storage.get(id);
+};
+
+exports.getAll = () => {
+    return Array.from(storage.values());
+};
